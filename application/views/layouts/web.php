c36432200dfd4c15d4ec350e64d650be91f18095
Daniel Hoyos <danielhoyos350@gmail.com>
2018-09-14 22:38:52 -0500

    Configuración de bootstrap en el layaout Web

--- application/views/layouts/web.php
+++ application/views/layouts/web.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Fundación Salvando Sueños</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="public/css/style.css">
 </head>
 <body>
     <!-- Header -->
@@ -14,5 +18,11 @@
 
     <!-- Footer -->
     <?php  include_once APPPATH. 'views/templates/footer.php'; ?>
+
+    <!-- Scripts -->
+    <script src="public/js/jquery-3.2.1.slim.min.js"></script>
+    <script src="public/js/popper.min.js"></script>
+    <script src="public/js/bootstrap.min.js"></script>
+    <script src="public/js/scripts.js"></script>
 </body>
 </html>
