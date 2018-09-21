999c38fffbea069065fb53aa69638f88700a82c3
alexis <bachilito7@misena.edu.co>
2018-09-20 22:29:09 -0500

    Inicion de pagina

--- application/views/layouts/web.php
+++ application/views/layouts/web.php
@@ -1,28 +1,34 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Fundación Salvando Sueños</title>
-
-    <!-- CSS -->
-    <link rel="stylesheet" href="public/css/bootstrap.min.css">
-    <link rel="stylesheet" href="public/css/style.css">
-</head>
+<?php include_once('application/views/templates/header.php') ?>
+<?php include_once('application/views/templates/nav.php') ?>
+<link rel="stylesheet" href="public/css/bootstrap.min.css">
 <body>
-    <!-- Header -->
-    <?php  include_once APPPATH. 'views/templates/header.php'; ?>
+<script>
+var cont=0
+function cambia() {
+    cont++;
+    switch(cont%4+1)
+    {
+        case 1:
+        document.getElementById("fotocambia").src="http://4.bp.blogspot.com/-0uVYopBOBec/WMf1CmL85WI/AAAAAAAAE0U/jt1rxp2Qc4YjeLuRqThGBx0yQcRuGXjSwCK4B/s000/amor-te-extrano-8.jpg";
+        break;
+        case 2:
+        document.getElementById("fotocambia").src="http://4.bp.blogspot.com/-0uVYopBOBec/WMf1CmL85WI/AAAAAAAAE0U/jt1rxp2Qc4YjeLuRqThGBx0yQcRuGXjSwCK4B/s000/amor-te-extrano-8.jpg";
+        break;
+        case 3:
+        document.getElementById("fotocambia").src="http://gentepasionyfutbol.com.co/home/wp-content/uploads/2018/07/matchball.jpg";
+        break;
+        case 4:
+        document.getElementById("fotocambia").src="http://e00-marca.uecdn.es/assets/multimedia/imagenes/2017/03/30/14909111932092.jpg";
+        break;
+    }
     
-    <?= $content_for_layout; ?>
+}
+function inicio() {
+    setInterval(cambia, 3000);
+    
+}
+window.onload=inicio;
 
-    <!-- Footer -->
-    <?php  include_once APPPATH. 'views/templates/footer.php'; ?>
-
-    <!-- Scripts -->
-    <script src="public/js/jquery-3.2.1.slim.min.js"></script>
-    <script src="public/js/popper.min.js"></script>
-    <script src="public/js/bootstrap.min.js"></script>
-    <script src="public/js/scripts.js"></script>
+</script>
+<img id="fotocambia" src="http://e00-marca.uecdn.es/assets/multimedia/imagenes/2017/03/30/14909111932092.jpg" style="width: 100%;">
 </body>
-</html>
--- application/views/templates/nav.php
+++ application/views/templates/nav.php
@@ -1,18 +1,40 @@
-<nav class="nav">
-    <li class="nav-item">
-        <a class="nav-link active" href="#">Inicio</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link active" href="#">Fundación</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link active" href="#">Plan Atención</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link active" href="#">Marco Legal</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link active" href="#">Contácto</a>
-    </li>
-    
+<nav id="nav" style="background-color: #44A4E2!important; " class="navbar navbar-toggler navbar-expand-lg navbar-light bg-light">
+<img src="<?= base_url().'public/img/logo.png'?>" style="width: 230px; height: 70px;">
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="i" style="color: black;"><b>Inicio</b></a>
+      </li>
+      <li class="nav-item" >
+        <a class="nav-link" href="#" style="color: black;"><b>Quienes somos</b></a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" style="color: black;" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+         <b>Dropdown</b>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown" style="color: black;">
+          <a class="dropdown-item" href="#"><b>Action</b></a>
+          <a class="dropdown-item" href="#"><b>Another action</b></a>
+          <!-- <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div> -->
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" style="color: black;"><b>Disabled</b></a>
+      </li>
+      <li>
+        <a href="#" title="Facebook">
+            <img src="<?= base_url().'public/img/facebook.png'?>" style="width: 28px; height: 24px;">
+        </a>
+      </li>|
+      <li>
+        <a href="#" title="Gmail">
+            <img src="<?= base_url().'public/img/correo.png'?>" style="width: 28px; height: 24px;">
+        </a>
+      </li>
+    </ul>
+  </div>
 </nav>
