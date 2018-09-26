f60615ccb034303a0eac8282e9a5c8e872bf19eb
Daniel Hoyos <danielhoyos350@gmail.com>
2018-09-26 11:02:37 -0500

    Creación de funcionalidad de restauración de contraseña con correo electrónico libreria PHPMailer

--- application/controllers/Usuario.php
+++ application/controllers/Usuario.php
@@ -4,12 +4,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Usuario extends CI_Controller{
     public function __construct(){
         parent::__construct();
+        $this->load->library("Auth");
     }
     
     public function registrar(){
         echo "Vamos a registrar un usuario";
     }
-
 }
 
 ?>
